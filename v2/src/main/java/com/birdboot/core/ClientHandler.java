be2350a6e384f955a818a4714a214e97c207d0ed
Beeeee <beeeee2@outlook.com>
2023-08-01 11:42:53 +0800

    Create

---
+++ v2/src/main/java/com/birdboot/core/ClientHandler.java
@@ -0,0 +1,32 @@
+package com.birdboot.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+/**
+ * 对指定客户端进行HTTP交流
+ */
+public class ClientHandler implements Runnable {
+    private Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+
+        //读取浏览器发送的内容
+        try {
+            InputStream is = socket.getInputStream();
+            int d;
+            while ((d = is.read()) != -1) {
+                System.out.print((char) d);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
