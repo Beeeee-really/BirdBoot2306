a4c3bffd00a2e86baa0a43fb29424f46ae84a1e2
Beeeee <beeeee2@outlook.com>
2023-08-01 09:52:16 +0800

    .

--- v1/src/main/java/com/birdboot/core/BirdBootApplication.java
+++ v1/src/main/java/com/birdboot/core/BirdBootApplication.java
@@ -1,5 +1,6 @@
 package com.birdboot.core;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 
 /**
@@ -9,7 +10,13 @@ public class BirdBootApplication {
     private ServerSocket serverSocket;
 
     private BirdBootApplication() {
-
+        try {
+            System.out.println("正在启动服务器...");
+            serverSocket = new ServerSocket(8088);
+            System.out.println("服务器启动完毕!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void start() {
