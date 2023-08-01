dfdedd207eb5667b8c81414b4991abe98a65ba13
Beeeee <beeeee2@outlook.com>
2023-08-01 10:23:03 +0800

    Update ClientHandler.java

--- v1/src/main/java/com/birdboot/core/ClientHandler.java
+++ v1/src/main/java/com/birdboot/core/ClientHandler.java
@@ -1,4 +1,21 @@
 package com.birdboot.core;
 
-public class ClientHandler {
+import java.net.Socket;
+
+/**
+ * 对指定客户端进行HTTP交流
+ */
+public class ClientHandler implements Runnable {
+    private Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+
+        //读取浏览器发送的内容
+
+    }
 }
