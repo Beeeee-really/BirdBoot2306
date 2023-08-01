17bfa38ee67780620b6d515226abd159ddf0bd9c
Beeeee <beeeee2@outlook.com>
2023-08-01 10:20:15 +0800

    .

--- v1/src/main/java/com/birdboot/core/BirdBootApplication.java
+++ v1/src/main/java/com/birdboot/core/BirdBootApplication.java
@@ -25,6 +25,9 @@ public class BirdBootApplication {
             System.out.println("等待客户端连接...");
             Socket socket = serverSocket.accept();
             System.out.println("一个客户端连接了!");
+
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ v1/src/main/java/com/birdboot/core/ClientHandler.java
@@ -0,0 +1,4 @@
+package com.birdboot.core;
+
+public class ClientHandler {
+}
