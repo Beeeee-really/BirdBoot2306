07c2eac509f44b25a2e73450632875578765ca2c
Beeeee <beeeee2@outlook.com>
2023-08-03 09:51:00 +0800

    Update

---
+++ v8/src/main/java/com/birdboot/core/BirdBootApplication.java
@@ -0,0 +1,45 @@
+package com.birdboot.core;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 启动主类
+ */
+public class BirdBootApplication {
+    private ServerSocket serverSocket;
+
+    private BirdBootApplication() {
+        try {
+            System.out.println("正在启动服务器...");
+            serverSocket = new ServerSocket(8088);
+            System.out.println("服务器启动完毕!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        try {
+
+            while (true) {
+                System.out.println("等待客户端连接...");
+                Socket socket = serverSocket.accept();
+                System.out.println("一个客户端连接了!");
+                ClientHandler handler = new ClientHandler(socket);
+                Thread t = new Thread(handler);
+                t.start();
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        BirdBootApplication birdBootApplication = new BirdBootApplication();
+        birdBootApplication.start();
+    }
+}
