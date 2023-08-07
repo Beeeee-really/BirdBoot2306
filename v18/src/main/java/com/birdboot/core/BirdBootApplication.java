3f278c6ea873b9e02bcaf6aed24932beb98b6e9c
Beeeee <beeeee2@outlook.com>
2023-08-07 17:57:30 +0800

    THE LAST UPDATE

--- v18/src/main/java/com/birdboot/core/BirdBootApplication.java
+++ v18/src/main/java/com/birdboot/core/BirdBootApplication.java
@@ -3,17 +3,21 @@ package com.birdboot.core;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * 启动主类
  */
 public class BirdBootApplication {
     private ServerSocket serverSocket;
+    private ExecutorService threadPool;
 
     private BirdBootApplication() {
         try {
             System.out.println("正在启动服务器...");
             serverSocket = new ServerSocket(8088);
+            threadPool = Executors.newFixedThreadPool(50);
             System.out.println("服务器启动完毕!");
         } catch (IOException e) {
             e.printStackTrace();
@@ -28,8 +32,7 @@ public class BirdBootApplication {
                 Socket socket = serverSocket.accept();
                 System.out.println("一个客户端连接了!");
                 ClientHandler handler = new ClientHandler(socket);
-                Thread t = new Thread(handler);
-                t.start();
+                threadPool.execute(handler);//交给线程池
             }
 
 
